2f9812e4ea178158c3758432e044079ce5dfe9a5
SwapnaSikder <swapnasikder55@gmail.com>
2024-04-18 23:05:38 +0600

    (Swapna Commit 02) Create Designs page in django and edit before page

--- ecommarce/ecommarceapp/admin.py
+++ ecommarce/ecommarceapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from ecommarceapp.models import Contact,Product
+from ecommarceapp.models import Contact,Design
 
 
 # Register your models here.
 admin.site.register(Contact)
-admin.site.register(Product)
+admin.site.register(Design)
