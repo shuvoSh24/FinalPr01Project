58d759ac9894ad7d356d7b9fc02d87ff3fa4b6c9
proma208 <humairaproma6@gmail.com>
2024-04-30 00:34:22 +0600

    (Humaira Commit 3) Design The  Designer Profile page

--- ecommarce/ecommarceapp/views.py
+++ ecommarce/ecommarceapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
-from ecommarceapp.models import Contact,Design,Orders,OrderUpdate
+from ecommarceapp.models import Contact, Design, Orders, OrderUpdate
 from django.contrib import messages
 from math import ceil
 from ecommarceapp import keys
 from django.conf import settings
-MERCHANT_KEY=keys.MK
-import json
-from django.views.decorators.csrf import  csrf_exempt
-from PayTm import Checksum
 
+MERCHANT_KEY = keys.MK
+import json
+from django.views.decorators.csrf import csrf_exempt
+from PayTm import Checksum
 
 
 # Create your views here.
@@ -28,8 +28,6 @@ def index(request):
     return render(request, "index.html", params)
 
 
-
-
 # 9-12 must be cmnt out after 15- line when uncomment
 # def contact(request):
 #     return render(request,"contact.html")
@@ -38,19 +36,20 @@ def index(request):
 
 
 def contact(request):
-    if request.method=="POST":
-        name=request.POST.get("name")
-        email=request.POST.get("email")
-        desc=request.POST.get("desc")
-        pnumber=request.POST.get("pnumber")
-        myquery=Contact(name=name,email=email,desc=desc,phonenumber=pnumber)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        desc = request.POST.get("desc")
+        pnumber = request.POST.get("pnumber")
+        myquery = Contact(name=name, email=email, desc=desc, phonenumber=pnumber)
         myquery.save()
-        messages.info(request,"we will get back to you soon..")
-        return render(request,"contact.html")
-    return render(request,"contact.html")
+        messages.info(request, "we will get back to you soon..")
+        return render(request, "contact.html")
+    return render(request, "contact.html")
+
 
 def about(request):
-    return render(request,"about.html")
+    return render(request, "about.html")
 
 
 def checkout(request):
@@ -129,3 +128,7 @@ def handlerequest(request):
         else:
             print('order was not successful because' + response_dict['RESPMSG'])
     return render(request, 'paymentstatus.html', {'response': response_dict})
+
+
+def profile(request):
+    return render(request,"profile.html")
